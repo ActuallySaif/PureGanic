917d99b07b2d08e2e7c9a4ef20cea8d5d7f10529
saifaldin hossam <actually.saif@gmail.com>
2025-11-20 03:57:59 +0200

    products section done :)

--- productScroll.js
+++ productScroll.js
@@ -1,5 +1,7 @@
 let drinksModel = document.getElementById("bottleReveal");
 let yogurtsModel = document.getElementById("yogurtReveal");
+let canModel = document.getElementById("canReveal");
+let appleModel = document.getElementById("appleReveal");
 
 drinksModel.addEventListener('load', () => {
 
@@ -9,7 +11,7 @@ drinksModel.addEventListener('load', () => {
 
     window.addEventListener('scroll', () => {
 
-        let drinksSec = document.getElementById("prodBlockId");
+        let drinksSec = document.getElementById("prodBlockId1");
         const rect = drinksSec.getBoundingClientRect();
         const windowHeight = window.innerHeight;
 
@@ -30,14 +32,57 @@ yogurtsModel.addEventListener('load', () => {
 
     window.addEventListener('scroll', () => {
 
-        let drinksSec = document.getElementById("prodBlockId");
-        const rect = drinksSec.getBoundingClientRect();
+        let yogurtSec = document.getElementById("prodBlockId2");
+        const rect = yogurtSec.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        let progress = 1 - (rect.bottom / (windowHeight + rect.height));
+        progress = Math.min(Math.max(progress, 0), 1);
+
+        yogurtsModel.currentTime = progress * animDuration;
+
+    });
+
+});
+
+
+canModel.addEventListener('load', () => {
+
+    canModel.animationName = "CanReveal";
+
+    let animDuration = 1;
+
+    window.addEventListener('scroll', () => {
+
+        let canSec = document.getElementById("prodBlockId3");
+        const rect = canSec.getBoundingClientRect();
         const windowHeight = window.innerHeight;
 
         let progress = 1 - (rect.bottom / ((windowHeight ) + rect.height));
         progress = Math.min(Math.max(progress, 0), 1);
 
-        yogurtsModel.currentTime = progress * animDuration;
+        canModel.currentTime = progress * animDuration;
+
+    });
+
+});
+
+appleModel.addEventListener('load', () => {
+
+    appleModel.animationName = "AppleReveal";
+
+    let animDuration = 1;
+
+    window.addEventListener('scroll', () => {
+
+        let appleSec = document.getElementById("prodBlockId4");
+        const rect = appleSec.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        let progress = 1 - (rect.bottom / ((windowHeight ) + rect.height));
+        progress = Math.min(Math.max(progress, 0), 1);
+
+        appleModel.currentTime = progress * animDuration;
 
     });
 
